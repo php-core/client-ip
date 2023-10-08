79fdba44350c3cb559153e2fce7ff0bae419e6fd
iSecNew10 <git@isecnew10.com>
2023-10-09 01:55:41 +0300

    Initial composer commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # client-ip
+
 Get client IP in PHP even behind CloudFlare, Proxy or Load-Balancer.
+
+
+# Licence
+
+MIT
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '0.0.1',
+        'version' => '0.0.1.0',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'php-core/client-ip',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'php-core/client-ip' => array(
+            'pretty_version' => '0.0.1',
+            'version' => '0.0.1.0',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
